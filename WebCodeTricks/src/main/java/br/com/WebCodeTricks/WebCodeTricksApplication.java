ed99f840909305d32ce607c7345b4d4e1756c088
Adolfo Poiatti <adolfo.poiatti@fatec.sp.gov.br>
2020-08-15 20:14:16 -0300

    testando envio

--- WebCodeTricks/src/main/java/br/com/WebCodeTricks/WebCodeTricksApplication.java
+++ WebCodeTricks/src/main/java/br/com/WebCodeTricks/WebCodeTricksApplication.java
@@ -4,9 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -14,10 +19,17 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaRepositories(basePackages="br.com.WebCodeTricks.repository")
 @EnableTransactionManagement
 @EntityScan(basePackages="br.com.WebCodeTricks.model")
-public class WebCodeTricksApplication {
+@EnableWebMvc
+
+
+public class WebCodeTricksApplication   {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebCodeTricksApplication.class, args);
 	}
 
+
 }
+
+
+	
